eb336a34f4e7b0e13d279f4face76d0023a8a1e8
wuyang475 <42758464+wuyang475@users.noreply.github.com>
2019-09-17 21:18:34 +0800

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,20 @@
+public class People {
+    private int Id;
+    private String name;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
